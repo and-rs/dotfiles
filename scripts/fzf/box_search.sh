5100f1f4a87bdf89646e7f05e2778f12c76b132a
JuanBaut <deserd@protonmail.com>
2025-02-19 00:05:07 -0500

    tmux scripts

--- scripts/fzf/box_search.sh
+++ scripts/fzf/box_search.sh
@@ -9,7 +9,7 @@ directories+=(--hidden --exclude .git --exclude node_module --exclude .cache --e
 directories+=(--exact-depth 1)
 
 # Use the array to execute the fd command and pipe to fzf-tmux for fuzzy finding in a tmux pane
-selected_dir=$("${directories[@]}" | fzf --tmux='center,33%')
+selected_dir=$("${directories[@]}" | fzf --reverse --tmux='center,33%')
 
 if [ -n "$selected_dir" ]; then
     cd "$selected_dir" || exit
