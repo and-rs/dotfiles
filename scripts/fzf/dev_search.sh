440b76f87b006ea11c515d9e61b2a9285432058f
and-rs <deserd@protonmail.com>
2025-03-29 15:03:20 -0500

    better search script

--- scripts/fzf/dev_search.sh
+++ scripts/fzf/dev_search.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-cd "$HOME/dev" || exit
+cd "$HOME/vault" || exit
 
 directories=(fd --type d)
 
 # Add more flags
 directories+=(--hidden --exclude .git --exclude node_module --exclude .cache --exclude .npm --exclude .mozilla --exclude .meteor --exclude .nv)
-directories+=(--exact-depth 1)
+directories+=(--exact-depth 2)
 
 # Use the array to execute the fd command and pipe to fzf-tmux for fuzzy finding in a tmux pane
 selected_dir=$("${directories[@]}" | fzf --reverse --tmux='center,33%')
