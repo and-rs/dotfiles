597f31f376875d58ea93a46e4dd8e05583ef12fd
and-rs <deserd@protonmail.com>
2025-04-04 09:31:53 -0500

    clean up and .config updates

--- .config/home-manager/home.nix
+++ .config/home-manager/home.nix
@@ -1,11 +1,8 @@
 { ... }: {
-  imports = [ ./tooling.nix ./packages.nix ];
-
-  home.username = "dagger";
-  home.homeDirectory = "/home/dagger";
+  home.username = "and-rs";
+  home.homeDirectory = "/home/and-rs";
   home.stateVersion = "24.11";
 
-  #programs.zsh.enable = true;
   programs.neovim.defaultEditor = true;
   programs.home-manager.enable = true;
 }
