00e9f5da8c565b6e987af6aa250ebb8fba08a8de
and-rs <deserd@protonmail.com>
2025-05-08 20:37:04 -0500

    huge rofi improvements

---
+++ .config/rofi/launch-rofi.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+powermenu="$HOME/.config/rofi/power-menu.sh"
+
+rofi -show drun \
+    -modes "drun,calc, power:$powermenu --symbols-font 'Symbols Nerd Font Mono' --choices=shutdown/reboot/suspend/logout"
