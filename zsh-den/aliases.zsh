c49b648e5832ea13356147734d9054dfffb78b87
and-rs <deserd@protonmail.com>
2025-07-03 12:16:44 -0500

    added zsh function to copy dir tree structure with md blocks

--- zsh-den/aliases.zsh
+++ zsh-den/aliases.zsh
@@ -2,6 +2,44 @@
 
 DOTS="$HOME/vault/personal/dotfiles"
 
+alias ..="z .."
+alias nv="nvim"
+alias c="clear -x"
+alias reload="exec zsh"
+alias sw="stow -t $HOME"
+
+alias l="eza -liha"
+alias lt="eza -lihaT --git-ignore"
+
+# alias f=". $DOTS/scripts/fzf/search.sh"
+# alias s=". $DOTS/scripts/fzf/vault.sh"
+
+alias u-nixos="sudo nixos-rebuild switch --flake $HOME/vault/personal/nixos#default"
+alias u-darwin="sudo darwin-rebuild switch --flake $HOME/vault/personal/nix-darwin"
+
+alias ff="fastfetch --logo-color-1 red --file $DOTS/utils/ascii/spider2.txt"
+alias ffn="fastfetch --logo-color-1 red --file $DOTS/utils/ascii/spider2.txt --config neofetch"
+
+dirtree() {
+  if ! command -v eza &> /dev/null; then
+      echo "Error: 'eza' command not found. Please install eza to use this script."
+      exit 1
+  fi
+  CLIPBOARD_CMD=""
+  if command -v pbcopy &> /dev/null; then
+      CLIPBOARD_CMD="pbcopy"
+  elif command -v wl-copy &> /dev/null; then
+      CLIPBOARD_CMD="wl-copy"
+  else
+      echo "Error: No clipboard utility found. Please install 'pbcopy' (macOS) or 'xclip' (Linux)."
+      exit 1
+  fi
+  TREE_OUTPUT=$(eza -Ta --git-ignore)
+  FORMATTED_OUTPUT="\`\`\`\n${TREE_OUTPUT}\n\`\`\`"
+  echo -e "${FORMATTED_OUTPUT}" | ${CLIPBOARD_CMD}
+  echo "eza tree output with markdown code blocks copied to clipboard!"
+}
+
 zd() {
   local current_dir
   current_dir=$(pwd)
@@ -20,21 +58,3 @@ ze() {
   file=$(fzf --prompt="edit > " --reverse --info="right" --padding=1,0,0,1)
   [[ -n $file ]] && $EDITOR "$file"
 }
-
-alias ..="z .."
-alias nv="nvim"
-alias c="clear -x"
-alias reload="exec zsh"
-alias sw="stow -t $HOME"
-
-alias l="eza -liha"
-alias lt="eza -lihaT --git-ignore"
-
-# alias f=". $DOTS/scripts/fzf/search.sh"
-# alias s=". $DOTS/scripts/fzf/vault.sh"
-
-alias u-nixos="sudo nixos-rebuild switch --flake $HOME/vault/personal/nixos#default"
-alias u-darwin="sudo darwin-rebuild switch --flake $HOME/vault/personal/nix-darwin"
-
-alias ff="fastfetch --logo-color-1 red --file $DOTS/utils/ascii/spider2.txt"
-alias ffn="fastfetch --logo-color-1 red --file $DOTS/utils/ascii/spider2.txt --config neofetch"
