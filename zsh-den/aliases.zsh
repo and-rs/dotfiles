f784b3f4c5ef22bbe998d79561f7743fb116b45f
and-rs <deserd@protonmail.com>
2025-07-16 22:38:59 -0500

    fastfetch tuning (im going crazy)

--- zsh-den/aliases.zsh
+++ zsh-den/aliases.zsh
@@ -17,7 +17,7 @@ alias lt="eza -lihaT --git-ignore"
 alias u-nixos="sudo nixos-rebuild switch --flake $HOME/vault/personal/nixos#default"
 alias u-darwin="sudo darwin-rebuild switch --flake $HOME/vault/personal/nix-darwin"
 
-alias ff="fastfetch --logo-color-1 red --file $DOTS/utils/ascii/spider2.txt"
+alias ff="fastfetch --logo-color-1 cyan --file $DOTS/utils/ascii/spider2.txt"
 alias ffn="fastfetch --logo-color-1 red --file $DOTS/utils/ascii/spider2.txt --config neofetch"
 
 code () { VSCODE_CWD="$PWD" open -n -b "com.microsoft.VSCode" --args $* ;}
