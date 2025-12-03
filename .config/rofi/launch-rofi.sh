84295218001f62936f22b1a7e761bbd879d95f8a
and-rs <deserd@protonmail.com>
2025-12-03 14:27:16 -0500

    fixed rofi calc, usual wm changes

--- .config/rofi/launch-rofi.sh
+++ .config/rofi/launch-rofi.sh
@@ -3,4 +3,4 @@
 powermenu="$HOME/.config/rofi/power-menu.sh"
 
 rofi -show drun \
-    -modes "drun,calc, power:$powermenu --symbols-font 'Symbols Nerd Font Mono' --choices=shutdown/hibernate/reboot/suspend/logout"
+    -modes "drun,calc,power:$powermenu -qalc-binary='/run/current-system/sw/bin/qalc' --symbols-font 'Symbols Nerd Font Mono' --choices=shutdown/hibernate/reboot/suspend/logout"
