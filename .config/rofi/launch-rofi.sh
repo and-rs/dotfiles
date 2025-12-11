5777d995244c81b1655c4ba9bed92b0af77016aa
and-rs <deserd@protonmail.com>
2025-12-11 16:43:43 -0500

    rofi tweaks and opencode alias

--- .config/rofi/launch-rofi.sh
+++ .config/rofi/launch-rofi.sh
@@ -3,4 +3,5 @@
 powermenu="$HOME/.config/rofi/power-menu.sh"
 
 rofi -show drun \
-    -modes "drun,calc,power:$powermenu -qalc-binary='/run/current-system/sw/bin/qalc' --symbols-font 'Symbols Nerd Font Mono' --choices=shutdown/hibernate/reboot/suspend/logout"
+    -modes "drun,calc,power:$powermenu --symbols-font 'Symbols Nerd Font Mono' --choices=shutdown/hibernate/reboot/suspend/logout" \
+    -qalc-binary='/run/current-system/sw/bin/qalc'
